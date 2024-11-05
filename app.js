c8b1afb86f61beb05386a4086263491be64f7537
millevolteb <millevolteb@gmail.com>
2024-11-05 15:46:19 -0600

    sp3

--- app.js
+++ app.js
@@ -7,9 +7,10 @@
 //     </div>
 
 document.querySelector("#about_us").addEventListener("click", (event) => {
-    backing.innerHTML = ` <main>
-    <div class="main1">
+  backing.innerHTML = ` <main>
+    <div>
       <h1 class=" m-6 box has-background-success-10 has-text-white has-text-centered is-size-4">
         FaunaFind currently hosts animal information from three states in the US, whith hopes to grow in the future. Please see each state for a few select inhabitant species.
       </h1>
-    </div>
+    </div>`;
+});
